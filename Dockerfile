8d057d6f514cc16eef9d8c3c95da0d3c72a50989
Abi Hafshin <abi@hafs.in>
2016-09-13 19:08:57 +0700

    make it more classy

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,8 @@
-FROM nginx:alpine
+FROM python:3-alpine
 
 ADD hamidun.py /usr/bin/hamidun
-ADD hamidun.conf /etc/hamidun.conf
-ADD service /usr/bin/service
 
-RUN apk add --no-cache python py-pip && \
-    pip install docker-py && \
-    apk del py-pip && \
-    chmod +x /usr/bin/hamidun /usr/bin/service
+RUN pip install docker-py && \
+    chmod +x /usr/bin/hamidun
     
-CMD ["/usr/bin/service", "run"]
+CMD ["/usr/bin/hamidun"]
--- hamidun.py
+++ hamidun.py
@@ -1,48 +1,137 @@
 #!/usr/bin/env python
 
-from docker import Client
-from os import listdir
-from os.path import isfile, join
+import os
 import re
-import ConfigParser
+import time
+
+from os import listdir, environ
+from os.path import isfile, join
+from threading import Thread
+
+from docker import Client as DockerClient
+
+
+LABEL_UPSTREAM_NAME = 'org.hamidun.name'
+LABEL_UPSTREAM_PORT = 'org.hamidun.port'
+
+UPSTREAM_FILE_NAME = '000-upstream.conf'
+NGINX_DEFAULT_DOWN_PORT = 13579
+
+MODIFIED_MIN_DELTA = 5
+
 
 def main():
-  config = ConfigParser.ConfigParser()
-  config.read('/etc/hamidun.conf')
+  docker_host = get_env('DOCKER_HOST', 'unix://var/run/docker.sock')
+  docker = DockerClient(base_url=docker_host)
+
+  network_name = get_env('DOCKER_NETWORK_NAME', 'load_balancer')
+  network = Network(docker, network_name)
+
+  hamidun = Hamidun(docker, network)
   
-  docker_host = config.get('Docker', 'host') or 'unix://var/run/docker.sock'
-  docker_network_name = config.get('Docker', 'shared_network') or 'load_balancer'
+  config_writer = ConfigurationWriter()
+  config_writer.down_port = get_env('NGINX_DOWN_PORT', NGINX_DEFAULT_DOWN_PORT)
 
-  template_dir = config.get('NginX', 'templates') or '/etc/nginx/sites-template'
-  out_dir = config.get('NginX', 'output') or '/etc/nginx/sites-enabled'
-
-  upstreams = load_configuration(docker_host, docker_network_name)
+  template_dir = get_env('TEMPLATES_DIR', '/etc/nginx/sites-template')
+  out_dir = get_env('OUTPUT_DIR', '/etc/nginx/conf.d/')
   files = [f for f in listdir(template_dir) if isfile(join(template_dir, f))]
   for f in files:
-    process_file(join(template_dir, f), join(out_dir, f), upstreams)
+    config_writer.process_template_file(join(template_dir, f), join(out_dir, f))
+  
+  upstream_file = join(out_dir, UPSTREAM_FILE_NAME)
+  config_writer.upstreams = hamidun.read_docker()
+  config_writer.write_upstream(upstream_file)
+  
+  monitor_thread = MonitorThread(hamidun, config_writer, upstream_file)
+  monitor_thread.start()
+  for upstreams in hamidun.monitor_docker():
+    config_writer.upstreams = upstreams
+    monitor_thread.notify_changed()
   # finish
 
-def load_configuration(host, network_name):
-  result = {}
-  client = Client(base_url=host)
-  containers = client.containers(filters={'label': 'org.hamidun.name'})
-  for container in containers:
-    cid = container['Id']
-    info = client.inspect_container(cid)
+def get_env(name, default):
+  if name in environ:
+    return environ[name]
+  else:
+    return default
 
-    if not network_name in info['NetworkSettings']['Networks']:
-      print('invalid network setting for %s' % cid[:8])
-      continue
 
-    network = info['NetworkSettings']['Networks'][network_name]
-    ip = network['IPAddress']
+
+class Hamidun:
+  def __init__(self, docker, network):
+    self.docker = docker
+    self.network = network
+    self.upstreams = {}
+
+  def read_docker(self):
+    containers = self.docker.containers(filters={'label': LABEL_UPSTREAM_NAME})
+    self.upstreams = {}
+    for container in containers:
+      cid = container['Id']
+      try:
+        upstream = self.read_container(cid)
+      except Exception as e: 
+        print(e)
+      else:
+        self.add_upstream(upstream.name, cid, upstream)
+    return self.upstreams
+    
+  def monitor_docker(self):
+    event_filter = {
+      'Type': 'continer',
+      'status': ['start', 'kill']
+    }
+    for event in self.docker.events(filters=event_filter, decode=True):
+      if LABEL_UPSTREAM_NAME in event['Actor']['Attributes']:
+        cid = event['id']
+        status = event['status']
+        modified = False
+        print('container "%s" status chaged to %s' % (event['Actor']['Attributes']['name'], status))
+        if status == 'start':
+          try:
+            upstream = self.read_container(cid)
+          except Exception as e: 
+            print(e)
+          else:
+            modified = self.add_upstream(upstream.name, cid, upstream)
+        elif status == 'kill':
+          name = event['Actor']['Attributes'][LABEL_UPSTREAM_NAME]
+          modified = self.remove_upstream(name, cid)
+        if modified:
+          yield self.upstreams
+        
+  def add_upstream(self, name, cid, upstream):
+    if name in self.upstreams:
+      self.upstreams[name][cid] = upstream
+    else:
+      self.upstreams[name] = {cid: upstream}
+    return True
+      
+  def remove_upstream(self, name, cid):
+    if name in self.upstreams and cid in self.upstreams[name]:
+      self.upstreams[name].pop(cid)
+      return True
+    else:
+      return False
+  
+  def read_container(self, cid, limit=3):
+    info = self.docker.inspect_container(cid)
+
+    if not self.network.name in info['NetworkSettings']['Networks']:
+      if limit == 0:
+        raise Exception('invalid network setting for %s', cid[:8])
+      docker.connect_container_to_network(cid, self.network.id)
+      time.sleep(0.5)
+      return self.process_container(cid, limit - 1)
+
+    ip = info['NetworkSettings']['Networks'][self.network.name]['IPAddress']
 
     labels = info['Config']['Labels']
-    name = labels['org.hamidun.name']
+    name = labels[LABEL_UPSTREAM_NAME]
 
     port = 0
-    if 'org.hamidun.port' in labels:
-      port = int(labels['org.hamidun.port'])
+    if LABEL_UPSTREAM_PORT in labels:
+      port = int(labels[LABEL_UPSTREAM_PORT])
     else:
       for key in info['NetworkSettings']['Ports'].keys():
         (_port, proto) = key.split('/')
@@ -51,47 +140,110 @@ def load_configuration(host, network_name):
           break
 
     if port == 0:
-      print('WARNING: container %s have label %s but no opened port found' % (cid, label))
+      raise Exception('No port found')
     else:
-      upstream = {
-      'container': cid,
-      'address': ip,
-      'port': port
-      }
-      if name in result:
-        result[name].append(upstream)
+      return Upstream(name, ip, port, 'container id: %s' % cid[:8])
+
+
+  def reload_loadbalancer(self):
+    containers = self.docker.containers(filters={'label': 'org.hamidun.type=loadbalancer'})
+    for container in containers:
+      executable = self.docker.exec_create(container['Id'], 'nginx -s reload')
+      self.docker.exec_start(executable['Id'], true)
+
+
+class ConfigurationWriter:
+  def __init__(self):
+    self.upstream_used = set()
+    self.upstreams = {}
+    self.down_port = NGINX_DEFAULT_DOWN_PORT
+    
+  def process_template_file(self, in_name, out_name):
+    def repl(m):
+      name = m.group(1)
+      self.upstream_used.add(name)
+      return 'upstream_%s' % (name)
+    finder = re.compile(r"\{\{upstream\s+(\w+)\}\}")
+
+    with open(in_name, 'r') as fin:
+      with open(out_name, 'w') as fout:
+        for line in fin:
+          fout.write(re.sub(finder, repl, line))
+
+  def write_upstream(self, file_name):
+    down_server = '  server localhost:%d; # down' % self.down_port
+    with open(file_name, 'w') as fout:
+      for name in self.upstream_used:
+        fout.write('upstream upstream_%s {\n' % name)
+        if name in self.upstreams and len(self.upstreams[name]) > 0:
+          servers = self.upstreams[name]
+          fout.write('\n'.join([servers[id].to_nginx_server_line() for id in servers]))
+        else:
+          fout.write(down_server)
+        fout.write('\n}\n\n')
+      self.write_down_vhost(fout)
+      
+  def write_down_vhost(self, fout):
+    fout.write("""server {
+    listen localhost:%d;
+    default_type 'text/plain';
+    return 503 'Service Unavailable';
+}
+""" % self.down_port)
+      
+
+
+class MonitorThread(Thread):
+  def __init__(self, hamidun, config_writer, upstream_file):
+    super().__init__(daemon=True)
+    self.hamidun = hamidun
+    self.config_writer = config_writer
+    self.upstream_file = upstream_file
+    self.modified = False
+    self.last_modified = 0
+    
+  def run(self):
+    time.sleep(1)
+    while True:
+      if self.modified:
+        now = time.time()
+        delta = now - self.last_modified
+        if delta >= MODIFIED_MIN_DELTA:
+          self.modified = False
+          print("writing upstream")
+          self.config_writer.write_upstream(self.upstream_file)
+          self.hamidun.reload_loadbalancer()
+        else:
+          time.sleep(MODIFIED_MIN_DELTA - delta)
       else:
-        result[name] = [upstream]
-  return result
+        time.sleep(1)
+
+  def notify_changed(self):
+    self.last_modified = time.time()
+    self.modified = True
 
 
-def process_file(in_name, out_name, upstreams):
-  def repl(m):
-    name = m.group(1)
-    servers = []
-    if name in upstreams:
-      servers = upstreams[name]
+class Network:
+  def __init__(self, docker, name):
+    networks = docker.networks(names=[name])
+    if len(networks) > 0:
+      self.id = networks[0]['Id']
     else:
-      servers = [{'address': 'localhost', 'port': 9080, 'container': 'down'}]
-    return create_nginx_upstream(name, servers)
-  finder = re.compile(r"\{\{upstream\s+(\w+)\}\}")
+      network = docker.create_network(name)
+      self.id = network['Id']
+    self.name = name
 
-  with open(in_name, 'r') as fin:
-    with open(out_name, 'w') as fout:
-      for line in fin:
-        fout.write(re.sub(finder, repl, line))
-
-
-def create_nginx_upstream(name, servers):
-  builder = ['upstream %s {\n' % name]
-  builder.extend([format_server_line(server) for server in servers])
-  builder.append('}')
-  return ''.join(builder)
-
-
-def format_server_line(data):
-  return '  server %s:%d; # container id: %s\n' % (data['address'], data['port'], data['container'][:8])
+class Upstream:
+  def __init__(self, name, address, port, comment):
+    self.name = name
+    self.nginx_server_line = 'server %s:%d; # %s' % (address, port, comment)
+    self.address = address
+    self.port = port
+    self.comment = comment
+    
+  def to_nginx_server_line(self):
+    return self.nginx_server_line
 
 
-
-main()
+if __name__ == "__main__":
+  main()
